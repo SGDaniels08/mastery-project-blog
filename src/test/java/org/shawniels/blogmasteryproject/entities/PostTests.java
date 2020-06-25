f9a8f0899008eb752bdd277265b53dff20818d6e
Shawn Daniels <SGDaniels08@gmail.com>
2020-06-25 15:35:37 -0400

    Scaffolding re-added, PostTests and PostControllerTests begun

---
+++ src/test/java/org/shawniels/blogmasteryproject/entities/PostTests.java
@@ -0,0 +1,15 @@
+package org.shawniels.blogmasteryproject.entities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+public class PostTests {
+
+
+    // Arrangement
+
+    // Action
+
+    // Assertion
+
+}
