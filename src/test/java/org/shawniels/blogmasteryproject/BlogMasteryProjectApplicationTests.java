84b331a0f043cb9d9aab25703ffb98b57af0b965
SGDaniels08 <65172879+SGDaniels08@users.noreply.github.com>
2020-06-24 21:35:25 -0400

    Project created from Spring Initializr

---
+++ src/test/java/org/shawniels/blogmasteryproject/BlogMasteryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shawniels.blogmasteryproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogMasteryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
